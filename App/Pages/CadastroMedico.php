2a88735fe3c58cecbd48cce2ac82fb8316525b4b
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2018-04-24 20:45:42 -0300

    Alterações do David e Maicon

--- App/Pages/CadastroMedico.php
+++ App/Pages/CadastroMedico.php
@@ -1,30 +1,41 @@
 <?php
 use Unopar\Core\Crud;
-    if (isset($_POST['nome']) && isset($_POST['cpf']) && isset($_POST['crm']) && isset($_POST['uf']) && isset($_POST['areaat']) && isset($_POST['email']) && isset($_POST['cep']) && isset($_POST['numero'])&& isset($_POST['complemento'])&& isset($_POST['rua'])&& isset($_POST['cidade'])&& isset($_POST['tel']) )  {
-        $nome = $_POST['nome'];
-        $cpf = $_POST['cpf'];
-        $crm = $_POST['crm'];
-        $uf = $_POST['uf'];
-        $areaat = $_POST['areaat'];
-        $email = $_POST['email'];
-        $cep = $_POST['cep'];
-        $numero = $_POST['numero'];
-        $complemento = $_POST['complemento'];
-        $rua = $_POST['rua'];
-        $cidade = $_POST['cidade'];
-        $tel = $_POST['tel'];  
-        
-        $resultado = Crud::consultar("SELECT * FROM medico WHERE medico.cpf_med = '$cpf' AND medico.crm_med = '$crm'");
-        if(empty($resultado)){
-           $gravar = Crud::salvar("INSERT INTO medico (nome_med ,cpf_med, crm_med) VALUES ('$nome', '$cpf', '$crm')");
-           echo "Cadastrado com Sucesso !";
-        } else{
-           echo "Não foi possivel cadastrar babaca";
-        }
-    }   
+if (isset($_POST['nome']) && isset($_POST['cpf']) && isset($_POST['crm']) && isset($_POST['uf']) && isset($_POST['areaat']) && isset($_POST['email']) && isset($_POST['cep']) && isset($_POST['numero'])&& isset($_POST['rg'])&& isset($_POST['rua'])&& isset($_POST['cidade'])&& isset($_POST['tel']) )  {
+
+
+
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $rg = $_POST['rg'];
+    $crm = $_POST['crm'];
+    $uf = $_POST['uf'];
+    $areaat = $_POST['areaat'];
+    $email = $_POST['email'];
+    $cep = $_POST['cep'];
+    $numero = $_POST['numero'];    
+    $rua = $_POST['rua'];
+    $cidade = $_POST['cidade'];
+    $tel = $_POST['tel'];  
+    
+    $resultado = Crud::consultar("SELECT * FROM medico WHERE medico.cpf_med = '$cpf' AND medico.crm_med = '$crm'");
+    if(empty($resultado)){
+       $gravar = Crud::salvar("INSERT INTO medico (nome_med ,cpf_med, crm_med) VALUES ('$nome', '$cpf', '$crm')");
+       echo "Cadastrado com Sucesso !";
+   } else{
+       echo "Não foi possivel cadastrar babaca";
+   }
+}
+
+
+ 
+
+
+
+
 ?>
 
 <!DOCTYPE html>
+
 <script language="javascript">
   function TestaCPF(elemento) {
     vcpf = elemento.value;
@@ -66,25 +77,30 @@ use Unopar\Core\Crud;
 </script>
 <body>
 <h2>Cadastro de Médicos</h2>
+
 <form class="form-fluid" method="POST">
     <div class="row">
-            <div class="col-md-6 col-xs-2">
+            <div class="col-md-3 col-xs-2">
                 <div class="form-group">
                     <label for="">Nome:</label>
                     <input type="text" class="form-control" name="nome" placeholder="Nome" required="">
                 </div>
             </div>
-        
+        <div class="col-md-3 col-xs-3">
+            <div class="form-group">
+                <label for="">UF:</label>
+                    <select class="form-control" width="100%" required="" name="uf">
+                        <option value="1">AC</option>
+                        <option value="2">PR</option>
+                        <option value="3">SP</option>
+                    </select>
+            </div>
+        </div>
         <div class="col-md-3 col-xs-2">
             <div class="form-group">
                 <label for="">Area de Atuação:</label>
                     <select class="form-control" width="100%" required="" name="areaat">
                         <option>Ortopedia</option>
-						<option>Ginecologia</option>
-						<option>Cardiovascular</option>
-						<option>Pneumo</option>
-						<option>Pediatria</option>
-						<option>Neurologia</option>
                     </select>
             </div>
         </div>
@@ -115,7 +131,7 @@ use Unopar\Core\Crud;
         <div class="col-md- col-xs-3">
             <div class="form-group">
                 <label for="">RG:</label>
-                <input type="text" class="form-control" name="" placeholder="RG">
+                <input type="text" class="form-control" name="rg" placeholder="RG">
             </div>
         </div>
 
@@ -123,7 +139,7 @@ use Unopar\Core\Crud;
         <div class="col-md-3 col-xs-2">
             <div class="form-group">
                 <label for="">Email:</label>
-                <input type="text" class="form-control" name="email" placeholder="Email" required="">
+                <input type="email" class="form-control" name="email" placeholder="Email" required="">
             </div>
         </div>
     </div>
--- App/Pages/CadastroPaciente.php
+++ App/Pages/CadastroPaciente.php
@@ -4,59 +4,80 @@
 use Unopar\Core\Crud;
     
 // Verificando se os campos do formulário foram preenchidos
-if (isset($_POST['nome']) && isset($_POST['endereco'])
-	&& isset($_POST['telefone']) && isset($_POST['sexo'])
-	&& isset($_POST['rg']) && isset($_POST['cpf'])) {
+if  (isset($_POST['nome']) && isset($_POST['endereco']) 
+    && isset($_POST['est_civ']) && isset($_POST['sexo']) 
+    && isset($_POST['data_nasc']) && isset($_POST['etnia'])
+    && isset($_POST['tel_prim']) && isset($_POST['tel_sec'])
+    && isset($_POST['email']) && isset($_POST['sexo'])
+    && isset($_POST['cns']) && isset($_POST['valid_cart'])
+    && isset($_POST['nome_mae']) && isset($_POST['nome_pai'])
+    && isset($_POST['convenio']) && isset($_POST['dat_hr'])
+	&& isset($_POST['rg']) && isset($_POST['cpf'])
+    && isset($_POST['observacoes'])) {
 
 	// Definindo um array com os dados do formulário
 	$dadosCadastro = [
 
 		'nome' 		=> filter_input(INPUT_POST, 'nome'),
 		'endereco' 	=> filter_input(INPUT_POST, 'endereco'),
-		'telefone' 	=> filter_input(INPUT_POST, 'telefone'),
+		'tel_prim' 	=> filter_input(INPUT_POST, 'tel_prim'),
+        'tel_sec'   => filter_input(INPUT_POST, 'tel_sec'),
 		'sexo' 		=> filter_input(INPUT_POST, 'sexo'),
+        'est_civ'   => filter_input(INPUT_POST, 'etnia'),
+        'email'     => filter_input(INPUT_POST, 'email'),
+        'cns'       => filter_input(INPUT_POST, 'cns'),
+        'valid_cart'=> filter_input(INPUT_POST, 'valid_cart'),
+        'nome_mae'  => filter_input(INPUT_POST, 'nome_mae'),
+        'nome_pai'  => filter_input(INPUT_POST, 'nome_pai'),
+        'convenio'  => filter_input(INPUT_POST, 'convenio'),
+        'dat_hr'    => filter_input(INPUT_POST, 'dat_hr'),
+        'observacoes'=> filter_input(INPUT_POST, 'observacoes'),
 		'rg' 		=> filter_input(INPUT_POST, 'rg'),
 		'cpf' 		=> filter_input(INPUT_POST, 'cpf')
 	];
 
 	// Variável que está armazenando a query com os dados do formulário
-	$sql = "INSERT INTO paciente(nome, endereco, telefone, sexo, rg, cpf) VALUES(
-		'{$dadosCadastro['nome']}', '{$dadosCadastro['endereco']}',
-		'{$dadosCadastro['telefone']}', '{$dadosCadastro['sexo']}',
-		'{$dadosCadastro['rg']}', '{$dadosCadastro['cpf']}')";
+	
 
 	// Acessando o método 'salvar' da classe Crud e passando como parâmetro a nossa query
 	// Se retornar verdadeiro então quer dizer que foi inserido no banco de dados com sucesso
-	if (Crud::salvar($sql)) {
-        
-        // Chamando o sweet alert
-		echo "<script>swal('Paciente cadastrado com sucesso!', '', 'success');</script>";
-	} else {
+	$gravar = Crud::salvar("INSERT INTO paciente(nome, data_nasc, sexo, etnia, cpf, rg, tel_prim, tel_sec, endereco, email, est_civ, cns, valid_cart, nome_mae, nome_pai, convenio, dat_hr, observacoes) VALUES(
+        '{$dadosCadastro['nome']}', '{$dadosCadastro['data_nasc']}',
+        '{$dadosCadastro['sexo']}', '{$dadosCadastro['etnia']}',
+        '{$dadosCadastro['cpf']}', '{$dadosCadastro['rg']}'
+        '{$dadosCadastro['tel_prim']}', '{$dadosCadastro['tel_sec']}',
+        '{$dadosCadastro['endereco']}', '{$dadosCadastro['email']}',
+        '{$dadosCadastro['est_civ']}', '{$dadosCadastro['cns']}',
+        '{$dadosCadastro['valid_cart']}', '{$dadosCadastro['nome_mae']}',
+        '{$dadosCadastro['nome_pai']}', '{$dadosCadastro['convenio']}',
+        '{$dadosCadastro['dat_hr']}', '{$dadosCadastro['observacoes']}')");
 
-		echo "<script>swal('Erro', 'paciente já cadastrado', 'error')</script>";
-	}
-    } else {
-	// echo 'Preencha todos os campos por favor';
-    }
-// Vamos gerar a consulta do banco de dados
+
+       
+   }
     
 ?>
 <h2>Cadastro de Pacientes</h2>
 <hr>
 <form class="form-fluid">
     <div class="row form-group">
-        <div class="col-md-6">
+        <div class="col-md-3">
             <label for="">Nome: </label>
-            <input type="text" class="form-control" placeholder="Nome">
+            <input type="text" class="form-control" placeholder="Nome" name="nome">
+        </div>
+        <div class="col-md-3">
+            <label for="">Sobrenome: </label>
+            <input type="text" class="form-control" placeholder="Sobrenome">
         </div>
         <div class="col-md-3">
             <label for="">Data de Nascimento: </label>
-            <input type="date" class="form-control" placeholder="Idade">
+            <input type="date" class="form-control" placeholder="Idade" name="data_nasc">
         </div>
         <div class="col-md-3">
             <label for="">Sexo: </label><br>
-            <label class="radio-inline"><input type="radio" name="optradio">Masculino</label>
-            <label class="radio-inline "><input type="radio" name="optradio">Feminino</label>
+            <label class="radio-inline"><input type="radio" name="optradio" value="m">Masculino</label>
+            <label class="radio-inline "><input type="radio" name="optradio" value="f">Feminino</label>
+            
         </div>
     </div>
     <div class="row form-group">
@@ -64,27 +85,29 @@ if (isset($_POST['nome']) && isset($_POST['endereco'])
             <label for="exampleFormControlSelect1">Etnia:</label>
             <select class="form-control" id="exampleFormControlSelect1">
                     <option>Selecione</option>
-                    <option>Branco</option>
-                    <option>Pardo</option>
-                    <option>Negro</option>
+                    <option value="Branco">Branco</option>
+                    <option value="Pardo">Pardo</option>
+                    <option value="Negro">Negro</option>
+                    <option value="Amarelo">Amarelo</option>
+                    <option value="Indigena">Indigena</option>
                 </select>
         </div>
 
         <div class="col-md-2">
             <label for="">CPF: </label>
-            <input type="text" class="form-control" placeholder="CPF">
+            <input type="text" class="form-control" placeholder="CPF" name="cpf">
         </div>
         <div class="col-md-2">
             <label for="">RG: </label>
-            <input type="text" class="form-control" placeholder="RG">
+            <input type="text" class="form-control" placeholder="RG" name="rg">
         </div>
         <div class="col-md-3">
             <label for="">Telefone 1: </label>
-            <input type="text" class="form-control" placeholder="Telefone">
+            <input type="text" class="form-control" placeholder="Telefone" name="tel_prim">
         </div>
         <div class="col-md-3">
             <label for="">Telefone 2: </label>
-            <input type="text" class="form-control" placeholder="Celular">
+            <input type="text" class="form-control" placeholder="Celular" name="tel_sec">
         </div>
     </div>
 
@@ -93,57 +116,48 @@ if (isset($_POST['nome']) && isset($_POST['endereco'])
             <label for="">CEP: </label>
             <input type="text" class="form-control" placeholder="CEP">
         </div>
-		
-		<div class="col-md-2">
+
+
+        <div class="col-md-3">
+            <label for="">Logradouro:</label>
+            <input type="text" class="form-control" placeholder="Rua">
+        </div>
+        <div class="col-md-2">
+            <label for="">Nº:</label>
+            <input type="text" class="form-control" placeholder="Número">
+        </div>
+        <div class="col-md-2">
+            <label for="">Bairro:</label>
+            <input type="text" class="form-control" placeholder="Bairro">
+        </div>
+
+        <div class="col-md-3">
+            <label for="">Cidade:</label>
+            <input type="text" class="form-control" placeholder="Cidade">
+        </div>
+    </div>
+
+    <div class="row form-group">
+        <div class="col-md-2">
             <label for="">Estado:</label>
             <select id="inputState" class="form-control">
                     <option selected>Selecione</option>
                     <option>Paraná</option>
                 </select>
         </div>
-		
-		<div class="col-md-2">
-            <label for="">Cidade:</label>
-            <input type="text" class="form-control" placeholder="Cidade">
-        </div>
-		
-		<div class="col-md-4">
-            <label for="">Logradouro:</label>
-            <input type="text" class="form-control" placeholder="Logradouro">
-        </div>
-		
-		<div class="col-md-2">
-            <label for="">Nº:</label>
-            <input type="text" class="form-control" placeholder="Número">
-        </div>
-	   
-    </div>
 
-    <div class="row form-group">
-           
-        <div class="col-md-4">
-            <label for="">Bairro:</label>
-            <input type="text" class="form-control" placeholder="Bairro">
+        <div class="col-md-3">
+            <label for="">Rua:</label>
+            <input type="text" class="form-control" placeholder="Rua">
         </div>
-		
-		<div class="col-md-4">
+
+        <div class="col-md-3">
             <label for="">Complemento:</label>
             <input type="text" class="form-control" placeholder="Complemento">
         </div>
-		
-		<div class="col-md-4">
-		<label for="exampleFormControlInput1">E-mail </label>
-		<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com">
-		</div>      
-    </div>
-        
 
 
-        
-    
-    <div class="row form-group">
-	
-	<div class="col-md-2">
+        <div class="col-md-2">
             <label for="">Estado Civil:</label>
             <select id="inputState" class="form-control">
                     <option selected>Selecione</option>
@@ -154,14 +168,41 @@ if (isset($_POST['nome']) && isset($_POST['endereco'])
                 </select>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-2">
             <label for="">Cartão(Sus): </label>
             <input type="text" class="form-control" placeholder="CNS">
         </div>
-        
-        <div class="col-md-4">
+    </div>
+    <div class="row form-group">
+        <div class="col-md-3">
+            <label for="">Convênio:</label>
+            <select id="inputState" class="form-control">
+                    <option selected>Selecione</option>
+                    <option>...</option>
+                    <option>...</option>
+                    <option>...</option>
+                    <option>...</option>
+                </select>
+        </div>
+
+        <div class="col-md-3">
+            <label for="">Plano:</label>
+            <select id="inputState" class="form-control">
+                    <option selected>Plano</option>
+                    <option>...</option>
+                    <option>...</option>
+                    <option>...</option>
+                    <option>...</option>
+            </select>
+        </div>
+
+        <div class="col-md-3">
+            <label for="">Registro no Convênio: </label>
+            <input type="text" class="form-control" placeholder="Registro no Convênio" name="convenio">
+        </div>
+        <div class="col-md-3">
             <label for="">Valid. Carteira: </label>
-            <input type="date" class="form-control" placeholder="Valid. Carteira">
+            <input type="date" class="form-control" placeholder="Valid. Carteira" name="valid_cart">
         </div>
 
     </div>
@@ -169,22 +210,24 @@ if (isset($_POST['nome']) && isset($_POST['endereco'])
     <div class="row form-group">
         <div class="col-md-4">
             <label for="">Nome da Mãe: </label>
-            <input type="text" class="form-control" placeholder="Nome da Mãe">
+            <input type="text" class="form-control" placeholder="Nome da Mãe" name="nome_mae">
         </div>
         
         <div class="col-md-4">
             <label for="">Nome do Pai: </label>
-            <input type="text" class="form-control" placeholder="Nome da Mãe">
+            <input type="text" class="form-control" placeholder="Nome da Mãe" name="nome_pai">
         </div>
         
         <div class="col-md-4">
-          
+    <label for="exampleFormControlInput1">E-mail </label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com" name="email">
+  </div>      
     </div>
     
     <div class="row form-group">
      <div class="col-md-3">
             <label for="">Data/Hora: </label>
-            <input type="date" class="form-control" placeholder="Data/Hora">
+            <input type="date" class="form-control" placeholder="Data/Hora" name="dat_hr">
         </div>
     </div>
     
@@ -192,7 +235,7 @@ if (isset($_POST['nome']) && isset($_POST['endereco'])
     
        <div class="col-md-12">
   <label for="comment">Observações:</label>
-  <textarea class="form-control" rows="5" id="comment"></textarea>
+  <textarea class="form-control" rows="5" id="comment" name="observacoes"></textarea>
 </div>
         
     </div>
@@ -204,4 +247,5 @@ if (isset($_POST['nome']) && isset($_POST['endereco'])
             <input type="reset" class="btn btn-warning" value="Limpar">
         </div>
     </div>
+
 </form>
